ffc0d6c663f7751cb25825e859016fde62c78068
Kyla Aquino <116413309+KylaKurstienAquino@users.noreply.github.com>
2023-06-30 16:18:58 +0800

    changed codes

---
+++ Model/FollowedAccounts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class FollowedAccounts
+    {
+        public string follower { get; set; }
+        public string following { get; set; }
+    }
+}
