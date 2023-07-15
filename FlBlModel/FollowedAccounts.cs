415763b5f5939273dee52ff4ea9fa737f501d567
Nicamaeee21 <cabreradanica507@gmail.com>
2023-07-15 09:49:33 +0800

    Follow

--- FlBlModel/FollowedAccounts.cs
+++ FlBlModel/FollowedAccounts.cs
@@ -8,7 +8,30 @@ namespace FlBlModel
 {
     public class FollowedAccounts
     {
+        public FollowedAccounts(string followerUser, string studentUser)
+        {
+            FollowerUser = followerUser;
+            StudentUser = studentUser;
+        }
+
         public string follower { get; set; }
         public string following { get; set; }
+        public string FollowerUser { get; set; }
+        public string StudentUser { get; set; }
+
+        public static void Add(FollowedAccounts newAccounts)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static FollowedAccounts FirstOrDefault(Func<object, bool> value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Remove(FollowedAccounts accounts)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- FlBlRules/Follow.cs
+++ FlBlRules/Follow.cs
@@ -29,3 +29,42 @@ namespace FlBlRules
         }
     }
 }
+
+public class FollowData
+{
+    private List<FollowedAccounts> followedAccounts;
+
+    public FollowData()
+    {
+        followedAccounts = new List<FollowedAccounts>();
+    }
+
+    public void dataFollow(string followerUser, string studentUser)
+    {
+        if (followedAccounts.Any(r => r.FollowerUser == followerUser && r.StudentUser == studentUser))
+        {
+            Console.WriteLine("Already following.");
+            return;
+        }
+
+        FollowedAccounts newAccounts = new FollowedAccounts(followerUser, studentUser);
+        FollowedAccounts.Add(newAccounts);
+
+        Console.WriteLine($"User '{followerUser}' is now following '{studentUser}'.");
+    }
+
+    public void dataUnfollow(string followerUser, string studentUser)
+    {
+        FollowedAccounts accounts = followedAccounts.FirstOrDefault(r => r.FollowerUser == followerUser && r.StudentUser == studentUser);
+        
+        if (accounts != null)
+        {
+            FollowedAccounts.Remove(accounts);
+            Console.WriteLine($"User '{followerUser}' has unfollowed '{studentUser}'.");
+        }
+        else
+        {
+            Console.WriteLine("No existing followed account found.");
+        }
+    }
+}
